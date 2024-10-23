daf8bb8af86951b22fd8d536012ad6d6f3bab0c5
josematamoros917 <josematamoros917@gmail.com>
2024-10-23 17:03:23 -0300

    cambios en styles y scripts

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,11 @@ $(document).ready(function() {
         }, 1000);
     });
     
-    // Cambio de fondo del contenedor al tocar un párrafo
-    $('.scroll-container p').on('touchend', function() {
-        $('.scroll-container').css('background-color', 'rgba(100, 100, 100, 0.7)'); // Cambia el color de fondo
-        setTimeout(() => {
-            $('.scroll-container').css('background-color', 'rgba(0, 0, 0, 0.5)'); // Regresar al color original
-        }, 1000);
-    });
+    // // Cambio de fondo del contenedor al tocar un párrafo
+    // $('.scroll-container p').on('touchend', function() {
+    //     $('.scroll-container').css('background-color', 'rgba(100, 100, 100, 0.7)'); // Cambia el color de fondo
+    //     setTimeout(() => {
+    //         $('.scroll-container').css('background-color', 'rgba(0, 0, 0, 0.5)'); // Regresar al color original
+    //     }, 1000);
+    // });
 });
